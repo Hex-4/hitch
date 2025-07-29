fd6f1d337d8fc31483905f251db1ebc33e9fb84c
hex4 <121138331+Hex-4@users.noreply.github.com>
2025-07-28 21:47:47 -0600

    janky grappling

--- player.gd
+++ player.gd
@@ -9,35 +9,90 @@ extends CharacterBody2D
 @onready var hook = %Hook
 @onready var anchor = %Anchor
 
+
 var hooked = false
+var hook_point = Vector2.ZERO
+var max_rope_length = 60
+var climb_speed = 60
 
 func _physics_process(delta):
 	%HookRay.look_at(get_global_mouse_position())
 	
 	if Input.is_action_just_pressed("hook"):
 		if %HookRay.is_colliding():
-			hook.global_position = $HookRay.get_collision_point()
+			hook_point = %HookRay.get_collision_point()
 			hooked = true
-			var original_global_pos = global_position
-			reparent(anchor, false)
-			global_position = original_global_pos
+
 			
+			max_rope_length = global_position.distance_to(hook_point)
+			
+	if Input.is_action_just_released("hook"):
+		hooked = false
 		
-	if (!hooked):
-		velocity.y += gravity * delta
-		var dir = Input.get_axis("ui_left", "ui_right")
-		if dir != 0:
-			if !is_on_floor():
-				velocity.x = lerp(velocity.x, dir * speed, 0.4)
-			velocity.x = lerp(velocity.x, dir * speed, acceleration)
+		# Tangent boost on release
+		var dir_to_hook = (hook_point - global_position).normalized()
+		var tangent = Vector2(-dir_to_hook.y, dir_to_hook.x)
+		var release_boost = velocity.dot(tangent)
+
+		if release_boost > 0:
+			velocity += tangent * release_boost * 0.5  # convert rope tension into flight
+
+		
+	velocity.y += gravity * delta
+	var dir = Input.get_axis("left", "right")
+	if dir != 0:
+		if !is_on_floor():
+			velocity.x = lerp(velocity.x, dir * speed, 0.4)
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, friction)
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+		
+	if hooked:
+		max_rope_length += Input.get_axis("up", "down") * climb_speed * delta
+		var to_hook = global_position - hook_point
+		var dist = to_hook.length()
+		var dir_to_hook = to_hook.normalized()
+
+		
+		# generated with chatgpt ;c (but i forced myself to comment the code so I understand it)
+		
+		var radial = dir_to_hook
+		var radial_speed = velocity.dot(radial)
+		if dist > max_rope_length:
+			# snap to rope length and remove all radial movement
+			global_position = hook_point + dir_to_hook * max_rope_length
+			if radial_speed > 0:
+				velocity -= radial * radial_speed
 		else:
-			velocity.x = lerp(velocity.x, 0.0, friction)
+			# rope is slack, only block inward motion
+			if radial_speed < 0:
+				velocity -= radial * radial_speed
+				
+		if dist >= max_rope_length:
+			var tangent = Vector2(-dir_to_hook.y, dir_to_hook.x)
+			var tangential_speed = velocity.dot(tangent)
+
+			# Slightly reinforce tangential motion
+			var swing_boost = 1.1
+			velocity = tangent * tangential_speed * swing_boost
+
 			
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = jump_speed
-		move_and_slide()
+
+	
+	move_and_slide()
+	
+	
 	
 		
 func _process(delta: float) -> void:
-	print(position)
+	if hooked:
+		
+		$Line2D.visible = true
+		$Line2D.points = [Vector2.ZERO, $Line2D.to_local(hook_point)]
+	else:
+		$Line2D.visible = false
+	
 	
